ef37c3147922237c31f367b323016d553110b4ce
racarter1215 <racarter1215@gmail.com>
2020-07-07 18:47:20 -0700

    code challenge 401 number 1

---
+++ challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace array_shift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray;
+            int insertValue;
+            insertShiftArray(inputArray, insertValue);
+        }
+        static int insertShiftArray(int[] array, int value)
+        {
+            int[] newArray = new int[array.Length + 1];
+            int newValuePlacement = (array.Length + 1) / 2;
+            int indexPlace = 0;
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (i == newValuePlacement - 1)
+                {
+                    newArray[i] = value;
+                }
+                else if (i != newValuePlacement - 1)
+                {
+                    newArray[i] = array[indexPlace++];
+                }
+                else
+                {
+                    Console.WriteLine("Please try again");
+                }
+            }
+            return newArray;
+        }
+    }
+}
